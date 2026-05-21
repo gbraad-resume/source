f4589bba6a7805a5ce4d4691808728672595960d
Gerard Braad <me@gbraad.nl>
2026-05-21 21:41:28 +0800

    Add callouts filter

--- generate.sh
+++ generate.sh
@@ -1,32 +1,26 @@
-#/bin/bash
+#!/bin/bash
 
 generate() {
-	CONTAINER_RUNTIME ?= podman
+	local CONTAINER_RUNTIME=${CONTAINER_RUNTIME:-podman}
+	local ASSETS=$(realpath assets)
 
 	${CONTAINER_RUNTIME} pull ghcr.io/gbraad/docugen:latest
 	git clone https://github.com/gbraad-docs/document-generation-assets assets --depth 1
 	mkdir public
-	sed \
-		-e 's/^> \[!NOTE\]/> 🛈 **Note**/' \
-		-e 's/^> \[!TIP\]/> 💡 **Tip**/' \
-		-e 's/^> \[!IMPORTANT\]/> ❗ **Important**/' \
-		-e 's/^> \[!WARNING\]/> ⚠️ **Warning**/' \
-		-e 's/^> \[!CAUTION\]/> 🔥 **Caution**/' \
-		-i *.md
-	${CONTAINER_RUNTIME} run --rm -v ${PWD}:/workspace ghcr.io/gbraad/docugen:latest pandoc \
-		-t html5 -f markdown-citations --template assets/templates/resume-template.html --standalone --section-divs -o ./public/index.html ./index.md
-	${CONTAINER_RUNTIME} run --rm -v ${PWD}:/workspace ghcr.io/gbraad/docugen:latest pandoc \
-		-t html5 --template ./assets/templates/resume-template.html --standalone --section-divs -o ./public/resume.html ./resume.md
-	${CONTAINER_RUNTIME} run --rm -v ${PWD}:/workspace ghcr.io/gbraad/docugen:latest pandoc \
-		-t html5 --template ./assets/templates/resume-template.html --standalone --section-divs -o ./public/recommendations.html ./recommendations.md
+	${CONTAINER_RUNTIME} run --rm -v ${PWD}:/workspace -v ${ASSETS}:/assets -w /workspace ghcr.io/gbraad/docugen:latest pandoc \
+		-t html5 -f markdown-citations --template /assets/templates/resume-template.html --standalone --section-divs --lua-filter=/assets/filters/callouts.lua -o ./public/index.html ./index.md
+	${CONTAINER_RUNTIME} run --rm -v ${PWD}:/workspace -v ${ASSETS}:/assets -w /workspace ghcr.io/gbraad/docugen:latest pandoc \
+		-t html5 --template /assets/templates/resume-template.html --standalone --section-divs --lua-filter=/assets/filters/callouts.lua -o ./public/resume.html ./resume.md
+	${CONTAINER_RUNTIME} run --rm -v ${PWD}:/workspace -v ${ASSETS}:/assets -w /workspace ghcr.io/gbraad/docugen:latest pandoc \
+		-t html5 --template /assets/templates/resume-template.html --standalone --section-divs --lua-filter=/assets/filters/callouts.lua -o ./public/recommendations.html ./recommendations.md
 	cp ./gbraad-fc.png ./public/
 	${CONTAINER_RUNTIME} pull ghcr.io/gbraad/phantomjs:latest
-	${CONTAINER_RUNTIME} run --rm -v ${PWD}:/workspace ghcr.io/gbraad/phantomjs:latest \
-		./assets/scripts/topdf.js ./public/resume.html ./public/resume.pdf 0.7
-	${CONTAINER_RUNTIME} run --rm -v ${PWD}:/workspace ghcr.io/gbraad/phantomjs:latest \
-		./assets/scripts/topdf.js ./public/resume.html ./public/resume-A5.pdf 0.7 A5
-	${CONTAINER_RUNTIME} run --rm -v ${PWD}:/workspace ghcr.io/gbraad/phantomjs:latest \
-		./assets/scripts/topdf.js ./public/recommendations.html ./public/recommendations.pdf 0.7
+	${CONTAINER_RUNTIME} run --rm -v ${PWD}:/workspace -v ${ASSETS}:/assets -w /workspace ghcr.io/gbraad/phantomjs:latest \
+		/assets/scripts/topdf.js ./public/resume.html ./public/resume.pdf 0.7
+	${CONTAINER_RUNTIME} run --rm -v ${PWD}:/workspace -v ${ASSETS}:/assets -w /workspace ghcr.io/gbraad/phantomjs:latest \
+		/assets/scripts/topdf.js ./public/resume.html ./public/resume-A5.pdf 0.7 A5
+	${CONTAINER_RUNTIME} run --rm -v ${PWD}:/workspace -v ${ASSETS}:/assets -w /workspace ghcr.io/gbraad/phantomjs:latest \
+		/assets/scripts/topdf.js ./public/recommendations.html ./public/recommendations.pdf 0.7
 }
 
 generate
